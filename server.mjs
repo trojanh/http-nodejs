0e13f32274503fba4c028c239a81ed98ce9c10d6
Rajan Tiwari <22974490+trojanh@users.noreply.github.com>
2022-12-22 17:32:22 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -19,7 +19,7 @@ const server = http.createServer((req, res) => {
       }), {})
       console.log({normalizedData});
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(normalizedData));
+      res.end(normalizedData);
     });
   } else {
     res.end('send a POST request');
