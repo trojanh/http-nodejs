c2123e92923b78ffcccf5580e5efc891dc393fbf
Rajan Tiwari <22974490+trojanh@users.noreply.github.com>
2022-12-22 16:49:10 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -11,7 +11,13 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       body = Buffer.concat(body).toString();
       const data = JSON.parse(body);
-      console.log(data);
+      console.log({data});
+      
+      const normalizedData = Object.keys(data).filter(t => t.match(/^q\d+_.*$/)).reduce((acc, curr) => ({
+        ...acc,
+        [curr.split('_')[1]]: x[curr]
+      }), {})
+      console.log({normalizedData});
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(data));
     });
