316a70121e3e14c417669b7a73b00bea6f332044
Rajan Tiwari <22974490+trojanh@users.noreply.github.com>
2022-12-22 14:43:16 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,14 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
+  if(req.body){
+    console.log({body: req.body})
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(req.body);
+  } 
+  else{
   res.write('Hello World!');
   res.end();
+  }
+  
 }).listen(process.env.PORT);
