ab75640f090908747eef6124a5c9b9a778b424ff
Rajan Tiwari <22974490+trojanh@users.noreply.github.com>
2022-12-22 15:11:43 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,14 +2,21 @@ import { createServer } from 'http';
 
 createServer((req, res) => {
   console.log("REQUEST ARRIVED", req)
-  if(req.body){
-    console.log({body: req.body})
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(req.body);
+ if(req.method == 'POST'){
+    
+        var jsonString;
+
+        req.on('data', function (data) {
+            jsonString = JSON.parse(data);
+        });
+
+        req.on('end', function () {
+            serverNext(req, res, JSON.stringify(jsonString));
+        });
   } 
   else{
   res.write('Hello World!');
-  res.end();
   }
+  res.end()
   
 }).listen(process.env.PORT);
